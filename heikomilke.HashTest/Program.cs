355723ead178125dd77f0c6d53d2c814d7d72d1c
Heiko Milke <hm@knm.io>
2019-11-15 15:18:15 +0100

    test

---
+++ heikomilke.HashTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Isopoh.Cryptography.Argon2;
+
+namespace heikomilke.HashTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            while (true)
+            {
+                string bogusPass = Guid.NewGuid().ToString();
+                Console.WriteLine("Creating hash for: "+bogusPass);
+                string hash= Argon2.Hash(
+                    password: bogusPass,
+                    type: Argon2Type.DataDependentAddressing,
+                    memoryCost: 4096
+                );
+                Console.WriteLine("Hash for: "+bogusPass + " is " + hash);
+                
+            }
+        }
+    }
+}
